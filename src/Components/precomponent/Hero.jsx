2558f1c87844bf9a56f1dfadbe379849f6a04dd0
luqmanofficial <lk3281751@gmail.com>
2024-10-11 18:08:51 +0500

    80done100

--- src/Components/precomponent/Hero.jsx
+++ src/Components/precomponent/Hero.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 import ce from "../../assets/anniversaryhero.png";
 import bg from '../../assets/herobackground.png';
+import { staggerContainer, fadeIn, planetVariants } from '../lib/motion';
+import { motion } from 'framer-motion';
+import styles from '../Style';
+
 
 const Hero = () => (
   <section
-    className='min-h-screen -mt-28 w-full flex flex-col md:flex-row justify-center items-center bg-cover'
+    className='min-h-screen -mt-10 w-full flex flex-col md:flex-row justify-center items-center bg-cover'
     style={{
       backgroundImage: `url(${bg})`,
     }}
   >
+     <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`w-full  flex lg:flex-row flex-col `}
+    >
 
+  <motion.div
+        variants={planetVariants('left')}
+        className={`w-1/2  `}
+
+      >
+
+         <img
+          src={ce}
+          alt="get-started"
+          className="w-[90%] h-[90%] "
+        /> 
+      </motion.div>
+      
+{/*     
 <div className='w-full md:w-1/2 flex justify-center items-center mt-4 p-10 md:mt-0'>
       <img src={ce} alt="Anniversary Celebration" className='max-w-full  h-auto' />
-    </div>
-    <div className='w-full md:w-1/2 flex flex-col ml-20 -mt-14 justify-center items-center text-center md:text-left'>
+    </div> */}
+ <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className={`w-1/2  flex justify-center items-center flex-col`}
+
+      >
+    {/* <div className='w-full  flex flex-col ml-20 -mt-14 justify-center items-center text-center md:text-left'> */}
     <h1
   className="text-4xl md:text-5xl font-bold mb-4 font-serif "
   style={{
@@ -24,15 +54,13 @@ const Hero = () => (
 >
   Celebrating 3 Years of Innovation at HoloModular
 </h1>
-      <p className='text-lg md:text-2xl text-justify mr-8 font-serif text-gray-200'  style={{
-    background: "linear-gradient(to right, #EDC967, #D2AC47, #F7EF8A)",
-    WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent",
-  }}>
+      <p className='text-lg md:text-2xl text-justify mr-8 font-serif text-gray-200'  >
         At HoloModular, we've spent the past three years shaping the future of technology. From artificial intelligence to cloud computing, augmented, and virtual reality, our passion for innovation drives us to push the boundaries of what's possible. Here's to many more years of groundbreaking achievements!
       </p>
-    </div>
-
+    {/* </div> */}
+    </motion.div>
+    </motion.div>
+    
   </section>
 );
 
